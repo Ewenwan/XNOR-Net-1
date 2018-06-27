25b1da6d922f134e8c262b5ce209ac9f401cc48e
Wan Youwen <1756429307@qq.com>
2018-06-27 11:23:53 +0800

    Update create_cifar10.sh

--- examples/cifar10/create_cifar10.sh
+++ examples/cifar10/create_cifar10.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env sh
 # This script converts the cifar data into leveldb format.
 
-EXAMPLE=examples/cifar10
-DATA=data/cifar10
-DBTYPE=lmdb
+EXAMPLE=examples/cifar10 # 需要存放转换后文件的地址
+DATA=data/cifar10        #源数据集地址
+DBTYPE=lmdb              # 转换后的格式
 
 echo "Creating $DBTYPE..."
-
+# 删除原来的
 rm -rf $EXAMPLE/cifar10_train_$DBTYPE $EXAMPLE/cifar10_test_$DBTYPE
-
+# 转换
 ./build/examples/cifar10/convert_cifar_data.bin $DATA $EXAMPLE $DBTYPE
 
 echo "Computing image mean..."
-
+# 计算均值
 ./build/tools/compute_image_mean -backend=$DBTYPE \
   $EXAMPLE/cifar10_train_$DBTYPE $EXAMPLE/mean.binaryproto
 

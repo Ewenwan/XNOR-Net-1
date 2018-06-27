83fe9f1afe68354450138f42a76bc84056154035
Wan Youwen <1756429307@qq.com>
2018-06-27 13:48:13 +0800

    Update train_conv2.sh

--- examples/cifar10_acc/train_conv2.sh
+++ examples/cifar10_acc/train_conv2.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env sh
 
-if [ ! -n "$1" ] ;then
-    echo "$1 is empty, default is 0"
-    gpu=0
-else
-    echo "use $1-th gpu"
-    gpu=$1
-fi
-
-
-TOOLS=./build/tools
+TOOLS=../../build/tools
 
 $TOOLS/caffe train \
-    --gpu=$gpu \
-    --solver=examples/cifar10_acc/cifar10_conv2.solver
+    --gpu=6,7 \
+    --solver=./cifar10_conv2.solver \
+    --weights=../cifar10/cifar10_full_iter_40000.caffemodel

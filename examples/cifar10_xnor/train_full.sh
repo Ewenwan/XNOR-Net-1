8b3ec2a825534ad67c4dd2818c9e297ecb57907e
Wan Youwen <1756429307@qq.com>
2018-06-27 14:34:35 +0800

    Update train_full.sh

--- examples/cifar10_xnor/train_full.sh
+++ examples/cifar10_xnor/train_full.sh
@@ -4,5 +4,5 @@ TOOLS=../../build/tools
 
 GLOG_log_dir=./log $TOOLS/caffe train \
     --gpu 6,7 \
-    --solver=./cifar10_full.solver
+    --solver=./cifar10_full.solver \
     --weights=../cifar10/cifar10_full_iter_40000.caffemodel

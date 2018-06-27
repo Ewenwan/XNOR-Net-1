472273f7d608d0a138b6429c69cdf0c105a91d9f
Wan Youwen <1756429307@qq.com>
2018-06-27 14:05:41 +0800

    Update and rename nobias_train.sh to train_full_nobias.sh

---
+++ examples/cifar10_binary/train_full_nobias.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+TOOLS=../../build/tools
+
+GLOG_log_dir=./log $TOOLS/caffe train \
+    --gpu=6,7 \
+    --solver=./cifar10_full.nobias.solver \
+    --weights=../cifar10/cifar10_full_iter_40000.caffemodel

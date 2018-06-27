bb8ef4ad9ad39d2646afc823b12e1d9279e955e4
Wan Youwen <1756429307@qq.com>
2018-06-27 11:48:25 +0800

    Update train_nobias.sh

--- examples/cifar10/train_nobias.sh
+++ examples/cifar10/train_nobias.sh
@@ -2,5 +2,5 @@
 
 TOOLS=./build/tools
 
-GLOG_log_dir=examples/cifar10/log $TOOLS/caffe train --gpu $1 \
+GLOG_log_dir=examples/cifar10/log $TOOLS/caffe train --gpu 4,5 \
     --solver=examples/cifar10/cifar10_full.nobias.solver

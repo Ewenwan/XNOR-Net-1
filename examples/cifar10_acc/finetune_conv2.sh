be24c6220d85befb5afbebdebd0d5eee8c044527
Wan Youwen <1756429307@qq.com>
2018-06-27 13:48:32 +0800

    Update finetune_conv2.sh

--- examples/cifar10_acc/finetune_conv2.sh
+++ examples/cifar10_acc/finetune_conv2.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env sh
-
-if [ ! -n "$1" ] ;then
-    echo "$1 is empty, default is 0"
-    gpu=0
-else
-    echo "use $1-th gpu"
-    gpu=$1
-fi
-
-TOOLS=./build/tools
+TOOLS=../../build/tools
 
 $TOOLS/caffe train \
-    --gpu=$gpu \
-    --solver=examples/cifar10_acc/cifar10_conv2.finetune.solver \
-    --weights=examples/cifar10/cifar10_full_iter_40000.caffemodel
+    --gpu=6,7 \
+    --solver=./cifar10_conv2.finetune.solver \
+    --weights=../cifar10/cifar10_full_iter_40000.caffemodel
